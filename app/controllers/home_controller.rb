7ad0de9134ec9935b6126536ff846f799fae61f2
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-10-17 19:50:48 -0600

    Ajax dummy call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @variable = params[:variable_test] || 'Original Value'
   end
 
   def do_something
+    @variable = params[:variable_test]
+    render text: @variable
   end
 end
