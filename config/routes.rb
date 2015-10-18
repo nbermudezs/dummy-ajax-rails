23f46b1cad2a8a409b319f41729199c22549f22a
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-10-17 19:50:23 -0600

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  post 'home#do_something'
+  post 'home/do_something'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
